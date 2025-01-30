764f086fbedda7c9555e336b153b30e776a4a337
Zelenskiy <zelensk1973@gmail.com>
2025-01-30 20:03:01 +0200

    fix: App to class

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Component } from 'react';
 import './App.css';
 import { MainPage } from './views/main-page';
 
-function App() {
-  return <MainPage />;
+class App extends Component {
+  render() {
+    return <MainPage />;
+  }
 }
 
 export default App;
