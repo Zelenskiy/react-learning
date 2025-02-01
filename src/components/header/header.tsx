466282287fa2c6e2c9876a0cc9dd154c93cb492a
Zelenskiy <zelensk1973@gmail.com>
2025-02-01 17:52:28 +0200

    fix: remove message from header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,15 +4,6 @@ import classes from './header.module.css';
 
 export class Header extends Component {
   render() {
-    return (
-      <header className={classes.header}>
-        To search, use: ability, berry, berry-firmness, berry-flavor,
-        contest-type, generation, item.
-        <br />
-        This API does not support server-side search, so I implemented group
-        search. Word part search is not supported. It&apos;s not my fault,
-        please don&apos;t lower the score.
-      </header>
-    );
+    return <header className={classes.header}></header>;
   }
 }
