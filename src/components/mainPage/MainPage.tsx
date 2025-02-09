88bf201ba9f7603c57c06872386ebc1149f9122b
Zelenskiy <zelensk1973@gmail.com>
2025-02-09 19:12:52 +0200

    feat: add url param

--- src/components/mainPage/MainPage.tsx
+++ src/components/mainPage/MainPage.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-compiler/react-compiler */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import classes from './main.module.css';
 import { Pokemon } from '../../services/types';
